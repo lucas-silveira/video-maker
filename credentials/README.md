ec7934d0b49382df589f0e87932c1ac8959c7b3b
Lucas <lugano1995@gmail.com>
2019-07-27 01:58:17 -0300

    feat: start text robot

---
+++ credentials/README.md
@@ -0,0 +1,11 @@
+# Credentials format
+
+## Algorithmia
+
+File: `algorithmia.json`
+
+```
+{
+    "apiKey": "simBt85WW25rCH3Ld6wx2rvPApG1"
+}
+```
--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-const readline = require('readline-sync');
+const readline = require('readline-sync')
+const robots = {
+    text: require('./robots/text.js')
+}
 
-function start() {
-    const content = {};
+async function start() {
+    const content = {}
 
-    content.searchTerm = askAndReturnSearchTerm();
-    content.prefix = askAndReturnPrefix();
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
+    await robots.text(content)
 
     function askAndReturnSearchTerm() {
-        return readline.question('Type a Wikipedia search term: ');
+        return readline.question('Type a Wikipedia search term: ')
     }
 
     function askAndReturnPrefix() {
-        const prefixes = ['Who is', 'What is', 'The history of'];
-        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ');
-        const selectedPrefixText = prefixes[selectedPrefixIndex];
-        return selectedPrefixText;
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+        return selectedPrefixText
     }
 
-    console.log(content);
+    console.log(content)
 }
 
-start();
+start()
