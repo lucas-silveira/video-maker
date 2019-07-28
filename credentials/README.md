0132e2dcd529c396e94de9b8b99313da00282f80
Lucas <lugano1995@gmail.com>
2019-07-28 02:04:15 -0300

    feat: add Watson natural language understanting service

--- credentials/README.md
+++ credentials/README.md
@@ -9,3 +9,18 @@ File: `algorithmia.json`
     "apiKey": "simBt85WW25rCH3Ld6wx2rvPApG1"
 }
 ```
+
+## Watson Natural Language Understanding
+
+File: `watson-nlu.json`
+
+```
+{
+  "apikey": "ZsfCBwOIzbukWxk4eXB4U2SyrmAmdXmFrREhyrQHazK8",
+  "iam_apikey_description": "Auto-generated for key befaf61c-813e-4dcc-8773-c4b3ab845513",
+  "iam_apikey_name": "Auto-generated service credentials",
+  "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
+  "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/7aca5b2514f549638f913ddb4bef16ce::serviceid:ServiceId-7e502a58-34a4-4939-8bf2-326fdb272656",
+  "url": "https://gateway.watsonplatform.net/natural-language-understanding/api"
+}
+```
--- robots/text.js
+++ robots/text.js
@@ -2,10 +2,21 @@ const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
 const sentenceBoundaryDetection = require('sbd')
 
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1.js')
+
+const nlu = new NaturalLanguageUnderstandingV1({
+    iam_apikey: watsonApiKey,
+    version: '2018-04-05',
+    url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+})
+
 async function robot(content) {
     await fetchContentFromWikipedia(content)
     sanitizeContent(content)
     breakContentIntoSentences(content)
+    limitMaximumSentences(content)
+    await fetchKeywordsOfAllSentences(content)
 
     async function fetchContentFromWikipedia(content) {
         const algorithmiaAuthenticated = algorithmia.client(algorithmiaApiKey)
@@ -50,6 +61,36 @@ async function robot(content) {
                 keywords: [],
                 images: []
             })
+        })
+    }
+
+    function limitMaximumSentences(content) {
+        content.sentences = content.sentences.slice(0, content.maximumSentences)
+    }
+
+    async function fetchKeywordsOfAllSentences(content) {
+        for (const sentence of content.sentences) {
+            sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+        }
+    }
+
+    async function fetchWatsonAndReturnKeywords(sentence) {
+        return new Promise( (resolve, reject) => {
+            nlu.analyze({
+                text: sentence,
+                features: {
+                    keywords: {}
+                }
+            })
+            .then( result => {
+                const keywords = result.keywords.map( (keyword) => {
+                    return keyword.text
+                })
+                resolve(keywords)
+            })
+            .catch( err => {
+                throw err
+            })        
         })
     }
 }
